72d5f82d6998db25b8a39eb6fbbe112db3206cb7
eric <eric.della@yahoo.com>
2020-09-02 17:30:15 -0700

    some of task 1, 2, 3

--- index.js
+++ index.js
@@ -2,44 +2,47 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *
+ * count is defined inside the function for counter1, outside the function for counter2
+ *
+ * 2. Which of the two uses a closure? How can you tell?
+ *
+ * counter2, it is looking for the variable "count" outside of the function
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,16 +54,24 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning() {
+  let runs = Math.floor(Math.random() * Math.floor(3));
+  return runs;
 }
+
+console.log(inning());
+
+let random = [inning(), inning(), inning(), inning()];
+var randomSum = random.reduce(function (a, b) {
+  return a + b;
+}, 0);
+console.log(random);
+
+console.log(randomSum);
 
 /* Task 3: finalScore()
 
@@ -74,13 +85,27 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
+function finalScore(c, innings) {
+  let finalScore = { Home: 0, Away: 0 };
+  const homeArray = [];
+  const awayArray = [];
+  for (let i = 0; i < innings; i++) {
+    homeArray.push(c());
+    finalScore.Home = homeArray.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    awayArray.push(c());
+    finalScore.Away = awayArray.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  }
 
-  /*Code Here*/
-
+  return finalScore;
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -102,9 +127,6 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
-
-
